03407222eaedbbe722e99c2ecd9391723454184a
Helder Balbino <helderbalbino@googlemail.com>
2023-03-28 16:36:32 +0100

    add teams and some styling changes to the dropdown bar

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -6,26 +6,43 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export default function BasicSelect() {
-  const [age, setAge] = React.useState('');
+  const [team, setTeam] = React.useState('');
 
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
+    setTeam(event.target.value);
   };
 
   return (
-    <Box sx={{ minWidth: 120 }}>
+    <Box sx={{ Width: 60 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <InputLabel id="demo-simple-select-label">Select your team</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={age}
-          label="Age"
+          value={team}
+          label="Select your team"
           onChange={handleChange}
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          <MenuItem value={1}>Arsenal</MenuItem>
+          <MenuItem value={2}>Aston Villa</MenuItem>
+          <MenuItem value={3}>Bournemouth</MenuItem>
+          <MenuItem value={4}>Brentford</MenuItem>
+          <MenuItem value={5}>Brighton</MenuItem>
+          <MenuItem value={6}>Chelsea</MenuItem>
+          <MenuItem value={7}>Crystal Palace</MenuItem>
+          <MenuItem value={8}>Everton</MenuItem>
+          <MenuItem value={9}>Fulham</MenuItem>
+          <MenuItem value={10}>Leeds</MenuItem>
+          <MenuItem value={11}>Leicester</MenuItem>
+          <MenuItem value={12}>Liverpool</MenuItem>
+          <MenuItem value={13}>Man City</MenuItem>
+          <MenuItem value={14}>Man United</MenuItem>
+          <MenuItem value={15}>Newcastle</MenuItem>
+          <MenuItem value={16}>Nottingham Forest</MenuItem>
+          <MenuItem value={17}>Southampton</MenuItem>
+          <MenuItem value={18}>Tottenham</MenuItem>
+          <MenuItem value={19}>West Ham</MenuItem>
+          <MenuItem value={20}>Wolves</MenuItem>
         </Select>
       </FormControl>
     </Box>
